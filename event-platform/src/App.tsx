65802ac9843abbec5c71c9a9b4a7980aee030283
luizcampos <felipe_luiz96@yahoo.com>
2022-06-24 22:10:44 -0300

    Final project new reposity to deploy in vercel

---
+++ event-platform/src/App.tsx
@@ -0,0 +1,17 @@
+import { Event } from "./pages/Event"
+import { Router } from "./Router"
+import { client } from './lib/apollo';
+import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+    </ApolloProvider>
+  )
+}
+
+export default App
